569369e8747b2b35fd7a9d04888fa399a703e738
aysel <soulll012@naver.com>
2022-05-17 13:06:53 +0900

    작업에 필요한 패키지 생성/수정

---
+++ src/main/java/com/ssafy/web/model/dto/User.java
@@ -0,0 +1,18 @@
+package com.ssafy.web.model.dto;
+
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@ApiModel(value = "UserDTO", description="유저 정보를 표현합니다.")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	private int id;
+	private String UserId;
+	private String pw;
+	private String email;
+	private String name;
+}
