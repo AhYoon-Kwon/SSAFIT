8fea3fbfa977b1cf525d8e2c7ccd006d7c530255
limhh0801 <limhh0801@DESKTOP-OM5FCKN>
2022-05-17 13:51:22 +0900

    Video.java Annotation 추가 VideoDao.java interface 추가

--- src/main/java/com/ssafy/web/model/dto/Video.java
+++ src/main/java/com/ssafy/web/model/dto/Video.java
@@ -1,5 +1,13 @@
 package com.ssafy.web.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Video {
 	private String vid;
 	private String url;
