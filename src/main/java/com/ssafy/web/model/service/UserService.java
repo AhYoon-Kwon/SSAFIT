4e2729a59dfb8311d63eca22cd30fb792d04f400
aysel <soulll012@naver.com>
2022-05-18 13:38:17 +0900

    Update Pom.xml, UserService, UserDao
    프로필 사진 업로드를 위한 file-upload, jwt를 위한 dependency 추가
    
    클래스 다이어그램대로 userDao, userService 작성

--- src/main/java/com/ssafy/web/model/service/UserService.java
+++ src/main/java/com/ssafy/web/model/service/UserService.java
@@ -3,6 +3,20 @@ package com.ssafy.web.model.service;
 import com.ssafy.web.model.dto.User;
 
 public interface UserService {
-	void join(User user, String msg) throws Exception;
-	User login(String id, String pw) throws Exception;
+	//회원가입
+	void join(User user);
+	//로그인
+	User login(String userId, String pw);
+	//비밀번호 변경
+	void changePassword(String oldPw, String newPw);
+	//프론트 계획 페이지 보니까 다른 회원정보 수정도 있길래 만들어봄
+	void changeUserInfo(User oldUser, User newUser);
+	//아이디 찾기
+	String findId(String email);
+	//비번 찾기
+	//근데 hash로 저장하면 비밀번호 찾는 것보다
+	//바로 비밀번호 변경 페이지로 넘어가는 게 나을지 고민중
+	String findPw(String email, String id);
+	//회원 탈퇴
+	void singOut(String userId, String pw);
 }
