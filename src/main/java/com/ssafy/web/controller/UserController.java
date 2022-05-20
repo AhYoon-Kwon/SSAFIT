c827e1d33b1df286feb2645a64d2cbedbcc882a5
aysel <soulll012@naver.com>
2022-05-21 04:43:33 +0900

    import 수정

--- src/main/java/com/ssafy/web/controller/UserController.java
+++ src/main/java/com/ssafy/web/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +22,6 @@ import com.ssafy.web.exception.WrongInfoException;
 import com.ssafy.web.model.dto.User;
 import com.ssafy.web.model.service.UserService;
 import com.ssafy.web.util.JWTUtil;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/user")
