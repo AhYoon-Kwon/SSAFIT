cd8e0658bcb0272c865c7a06a70eeb64412a1614
임현호 <limhh0801@naver.com>
2022-05-20 16:21:32 +0900

    cors

--- src/main/java/com/ssafy/web/configuration/WebConfig.java
+++ src/main/java/com/ssafy/web/configuration/WebConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.web.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,5 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer{
 	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*")
+		.allowedMethods("GET","POST","PUT","DELETE")
+		.maxAge(6000);
+	}
 	
 }
